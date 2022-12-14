06a074a4d6c056ea5130be74fae140a2a52921d0
Patil Khushal <patilkhushalp2803@gmail.com>
2022-12-14 21:45:22 +0530

    Updated Backgrounds for roots

--- TextToMorse.py
+++ TextToMorse.py
@@ -111,7 +111,7 @@ def newRoot1():
 
     root2.geometry("%dx%d" %(w, h))
     root2.title("Text 2 Morse-Code Module")
-    root2['bg'] = 'white'
+    root2['bg'] = 'black'
 
     root2.grid_rowconfigure(0, weight = 1)
     
@@ -123,19 +123,19 @@ def newRoot1():
     root2.grid_columnconfigure(1, weight = 2)
     root2.grid_columnconfigure(2, weight = 1)
 
-    Label0 = Label(root2, text = "Text 2 Morse-Code Module", bg = 'white', fg = 'blue', font=("Times New Roman bold", 50))
+    Label0 = Label(root2, text = "Text 2 Morse-Code Module", bg = 'black', fg = 'blue', font=("Times New Roman bold", 50))
     Label0.grid(row = 0, column = 0, columnspan = 3)
 
-    validateTextLabel = Label(root2, text = "", bg = 'white', fg = 'red', font=("Times New Roman bold", 15))
+    validateTextLabel = Label(root2, text = "", bg = 'black', fg = 'red', font=("Times New Roman bold", 15))
     validateTextLabel.grid(row = 2, column = 1)
 
-    Label2 = Label(root2, text = "Equivalent Morse Code", bg = 'white', fg = 'green', font=("Times New Roman bold", 20))
+    Label2 = Label(root2, text = "Equivalent Morse Code", bg = 'black', fg = 'green', font=("Times New Roman bold", 20))
     Label2.grid(row = 3, column = 0, sticky = E)
 
-    result = Label(root2, text = "", bg = 'white', width = 42, fg = 'red', font=("Times New Roman bold", 15))
+    result = Label(root2, text = "", bg = 'black', width = 42, fg = 'red', font=("Times New Roman bold", 15))
     result.grid(row = 3, column = 1)
 
-    Label1 = Label(root2, text = "Enter Text", bg = 'white', fg = 'green', font=("Times New Roman bold", 20)).grid(row = 1, column = 0, sticky = E)
+    Label1 = Label(root2, text = "Enter Text", bg = 'black', fg = 'green', font=("Times New Roman bold", 20)).grid(row = 1, column = 0, sticky = E)
     text = Entry(root2, font=("Times New Roman bold", 15))
     text.bind("<KeyRelease>", lambda event, arg = (text.get(), validateTextLabel, result, root2) : validateText(event, text.get().lower(), validateTextLabel, result, root2))
     text.bind("<BackSpace>", lambda event, arg = (text.get(), validateTextLabel, result, root2) : validateB(event, text.get().lower(), validateTextLabel, result, root2))
@@ -155,7 +155,7 @@ def newRoot2():
 
     root2.geometry("%dx%d" %(w, h))
     root2.title("Morse-Code 2 Text Module")
-    root2['bg'] = 'white'
+    root2['bg'] = 'black'
 
     root2.grid_rowconfigure(0, weight = 1)
     
@@ -167,19 +167,19 @@ def newRoot2():
     root2.grid_columnconfigure(1, weight = 2)
     root2.grid_columnconfigure(2, weight = 1)
 
-    Label0 = Label(root2, text = "Morse-Code 2 Text Module", bg = 'white', fg = 'blue', font=("Times New Roman bold", 50))
+    Label0 = Label(root2, text = "Morse-Code 2 Text Module", bg = 'black', fg = 'blue', font=("Times New Roman bold", 50))
     Label0.grid(row = 0, column = 0, columnspan = 3)
 
-    validateTextLabel = Label(root2, text = "", bg = 'white', fg = 'red', font=("Times New Roman bold", 15))
+    validateTextLabel = Label(root2, text = "", bg = 'black', fg = 'red', font=("Times New Roman bold", 15))
     validateTextLabel.grid(row = 2, column = 1)
 
-    Label2 = Label(root2, text = "Equivalent Text", bg = 'white', fg = 'green', font=("Times New Roman bold", 20))
+    Label2 = Label(root2, text = "Equivalent Text", bg = 'black', fg = 'green', font=("Times New Roman bold", 20))
     Label2.grid(row = 3, column = 0, sticky = E)
 
-    result = Label(root2, text = "", bg = 'white', width = 42, fg = 'red', font=("Times New Roman bold", 15))
+    result = Label(root2, text = "", bg = 'black', width = 42, fg = 'red', font=("Times New Roman bold", 15))
     result.grid(row = 3, column = 1)
 
-    Label1 = Label(root2, text = "Enter Morse Code", bg = 'white', fg = 'green', font=("Times New Roman bold", 20)).grid(row = 1, column = 0, sticky = E)
+    Label1 = Label(root2, text = "Enter Morse Code", bg = 'black', fg = 'green', font=("Times New Roman bold", 20)).grid(row = 1, column = 0, sticky = E)
     text = Entry(root2, font=("Times New Roman bold", 15))
     text.bind("<KeyRelease>", lambda event, arg = (text.get(), validateTextLabel, result) : morseToText(event, text.get().lower(), validateTextLabel, result))
     text.grid(row = 1, column = 1, ipadx = 150)
@@ -194,7 +194,7 @@ h = root.winfo_screenheight()
 
 root.geometry("%dx%d" %(w, h))
 root.title("Morse-Code Translator")
-root['bg'] = 'white'
+root['bg'] = 'black'
 
 root.grid_columnconfigure(0, weight = 1)
 root.grid_columnconfigure(1, weight = 1)
@@ -204,7 +204,7 @@ root.grid_rowconfigure(0, weight = 1)
 root.grid_rowconfigure(1, weight = 1)
 root.grid_rowconfigure(2, weight = 1)
 
-Label(root, text = "Morse-Code Translator", bg = 'white', fg = 'blue', font=("Times New Roman bold", 50)).grid(row = 0, column = 0, columnspan = 3)
+Label(root, text = "Morse-Code Translator", bg = 'black', fg = 'blue', font=("Times New Roman bold", 50)).grid(row = 0, column = 0, columnspan = 3)
 Button(root, bg = 'purple', fg = 'black', command = newRoot1, font=("Times New Roman bold", 20), text = "Text 2 Morse-Code").grid(row = 1, column = 0, sticky = E, ipadx = 50)
 Button(root, bg = 'purple', fg = 'black', command = newRoot2, font=("Times New Roman bold", 20), text = "Morse-Code 2 Text").grid(row = 1, column = 1, sticky = E, ipadx = 50)
 
